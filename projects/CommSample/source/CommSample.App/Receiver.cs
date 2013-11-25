07159f8d407e0944133bfd4ea24ceace7d79365d
Brian Rogers <bobbymcr@bobbymcr.com>
2013-11-25 15:13:23 -0800

    Skeleton of integration test app

---
+++ projects/CommSample/source/CommSample.App/Receiver.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Receiver.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CommSample
+{
+    using System;
+    using System.Threading.Tasks;
+
+    internal sealed class Receiver
+    {
+        private readonly MemoryChannel channel;
+
+        public Receiver(MemoryChannel channel)
+        {
+            this.channel = channel;
+        }
+
+        public Task RunAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ projects/CommSample/source/CommSample.App/Sender.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Sender.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CommSample
+{
+    using System;
+    using System.Threading.Tasks;
+
+    internal sealed class Sender
+    {
+        private readonly MemoryChannel channel;
+
+        public Sender(MemoryChannel channel)
+        {
+            this.channel = channel;
+        }
+
+        public Task RunAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
