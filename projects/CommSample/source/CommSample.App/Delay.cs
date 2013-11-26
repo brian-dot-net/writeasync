c86f1c3010e2d99ce094374863c92aff48efb554
Brian Rogers <bobbymcr@bobbymcr.com>
2013-11-26 09:10:37 -0800

    Add missing file Delay.cs

---
+++ projects/CommSample/source/CommSample.App/Delay.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Delay.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CommSample
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal sealed class Delay
+    {
+        private readonly int iterations;
+        private readonly int milliseconds;
+
+        private int current;
+
+        public Delay(int iterations, int milliseconds)
+        {
+            this.iterations = iterations;
+            this.milliseconds = milliseconds;
+        }
+
+        public void Next()
+        {
+            if (++this.current == this.iterations)
+            {
+                this.current = 0;
+                Thread.Sleep(this.milliseconds);
+            }
+        }
+    }
+}
