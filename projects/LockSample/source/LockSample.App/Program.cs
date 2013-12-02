d253f1a2560ebf38ff3bf3034e21f32d7f05b935
Brian Rogers <bobbymcr@bobbymcr.com>
2013-12-01 16:47:53 -0800

    Add basic integration test skeleton

--- projects/LockSample/source/LockSample.App/Program.cs
+++ projects/LockSample/source/LockSample.App/Program.cs
@@ -7,11 +7,68 @@
 namespace LockSample
 {
     using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     internal sealed class Program
     {
         private static void Main(string[] args)
         {
+            Random random = new Random();
+            ExclusiveLock l = new ExclusiveLock();
+            List<int> list = new List<int>();
+
+            using (CancellationTokenSource cts = new CancellationTokenSource())
+            {
+                Task task = LoopAsync(random, l, list, cts.Token);
+
+                Thread.Sleep(1000);
+
+                cts.Cancel();
+                task.Wait();
+            }
+        }
+
+        private static async Task LoopAsync(Random random, ExclusiveLock l, IList<int> list, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                switch (random.Next(1))
+                {
+                    case 0:
+                        await EnumerateListAsync(l, list);
+                        break;
+                }
+            }
+        }
+
+        private static async Task EnumerateListAsync(ExclusiveLock l, IList<int> list)
+        {
+            ExclusiveLock.Token token = await l.AcquireAsync();
+            await Task.Yield();
+            try
+            {
+                int lastItem = 0;
+                foreach (int item in list)
+                {
+                    if (lastItem != (item - 1))
+                    {
+                        throw new InvalidOperationException(string.Format(
+                            CultureInfo.InvariantCulture,
+                            "State corruption detected; expected {0} but saw {1} in next list entry.",
+                            lastItem + 1,
+                            item));
+                    }
+
+                    await Task.Yield();
+                }
+            }
+            finally
+            {
+                l.Release(token);
+            }
         }
     }
 }
