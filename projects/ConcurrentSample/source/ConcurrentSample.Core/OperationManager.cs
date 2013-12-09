1121744e8f4add989d48264d81a8a7d24ef3eeba
Brian Rogers <bobbymcr@bobbymcr.com>
2013-12-08 22:42:55 -0800

    First exception fails operation without executing remaining calls

--- projects/ConcurrentSample/source/ConcurrentSample.Core/OperationManager.cs
+++ projects/ConcurrentSample/source/ConcurrentSample.Core/OperationManager.cs
@@ -8,7 +8,6 @@ namespace ConcurrentSample
 {
     using System;
     using System.Collections.Generic;
-    using System.Threading;
     using System.Threading.Tasks;
 
     public class OperationManager
@@ -25,14 +24,40 @@ namespace ConcurrentSample
         public async Task RunAsync(int totalCalls)
         {
             AsyncSemaphore semaphore = new AsyncSemaphore(this.maxPendingCalls);
-            List<Task> pending = new List<Task>();
+            Queue<Task> pending = new Queue<Task>();
+            List<Exception> exceptions = new List<Exception>();
             for (int i = 0; i < totalCalls; ++i)
             {
                 await semaphore.WaitAsync();
-                pending.Add(this.CallAsync(semaphore, i));
+                pending.Enqueue(this.CallAsync(semaphore, i));
+                HandleCompletedCalls(pending, exceptions);
+                if (exceptions.Count > 0)
+                {
+                    throw new AggregateException(exceptions).Flatten();
+                }
             }
 
             await Task.WhenAll(pending);
+        }
+
+        private static void HandleCompletedCalls(Queue<Task> pending, IList<Exception> exceptions)
+        {
+            while (pending.Count > 0)
+            {
+                Task task = pending.Peek();
+                if (task.IsCompleted)
+                {
+                    pending.Dequeue();
+                    if (task.Exception != null)
+                    {
+                        exceptions.Add(task.Exception);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
         }
 
         private async Task CallAsync(AsyncSemaphore semaphore, int i)
--- projects/ConcurrentSample/test/ConcurrentSample.Test.Unit/OperationManagerTest.cs
+++ projects/ConcurrentSample/test/ConcurrentSample.Test.Unit/OperationManagerTest.cs
@@ -112,5 +112,40 @@ namespace ConcurrentSample.Test.Unit
             Assert.Equal(TaskStatus.RanToCompletion, task.Status);
             Assert.Equal(0, pending.Count);
         }
+
+        [Fact]
+        public void First_exception_fails_operation_without_executing_remaining_calls()
+        {
+            Queue<TaskCompletionSource<bool>> pending = new Queue<TaskCompletionSource<bool>>();
+            Func<Task> doAsync = delegate
+            {
+                TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+                pending.Enqueue(tcs);
+                return tcs.Task;
+            };
+
+            OperationManager manager = new OperationManager(1, doAsync);
+            Task task = manager.RunAsync(3);
+
+            Assert.False(task.IsCompleted);
+            Assert.Equal(1, pending.Count);
+
+            TaskCompletionSource<bool> current = pending.Dequeue();
+            InvalidProgramException exception = new InvalidProgramException("expected");
+            current.SetException(exception);
+
+            Assert.False(task.IsCompleted);
+            Assert.Equal(1, pending.Count);
+
+            current = pending.Dequeue();
+            current.SetResult(false);
+
+            Assert.True(task.IsCompleted);
+            Assert.True(task.IsFaulted);
+            Assert.NotNull(task.Exception);
+            AggregateException ae = Assert.IsType<AggregateException>(task.Exception).Flatten();
+            Assert.Equal(1, ae.InnerExceptions.Count);
+            Assert.Same(exception, ae.InnerExceptions[0]);
+        }
     }
 }
