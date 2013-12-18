e959ccff625ad229b78d6445bc0121b787f4107c
Brian Rogers <bobbymcr@bobbymcr.com>
2013-12-18 13:01:29 -0800

    Basic app skeleton

--- projects/ThreadSample/source/ThreadSample.App/Program.cs
+++ projects/ThreadSample/source/ThreadSample.App/Program.cs
@@ -7,11 +7,30 @@
 namespace ThreadSample
 {
     using System;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     internal sealed class Program
     {
         private static void Main(string[] args)
         {
+            string pipeName = Guid.NewGuid().ToString("N");
+            using (CancellationTokenSource cts = new CancellationTokenSource())
+            {
+                Receiver receiver = new Receiver(pipeName, true);
+                Sender sender = new Sender(pipeName, true);
+
+                Task receiveTask = receiver.ReceiveAsync(cts.Token);
+                Thread.Sleep(1000);
+
+                Task sendTask = sender.SendAsync(cts.Token);
+
+                Thread.Sleep(2000);
+
+                cts.Cancel();
+
+                Task.WaitAll(receiveTask, sendTask);
+            }
         }
     }
 }
