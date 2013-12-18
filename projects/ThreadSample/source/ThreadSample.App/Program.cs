6aa620febde231e6f13194fc0bedd004e60eae1a
Brian Rogers <bobbymcr@bobbymcr.com>
2013-12-18 13:11:02 -0800

    Add send-receive status reporting

--- projects/ThreadSample/source/ThreadSample.App/Program.cs
+++ projects/ThreadSample/source/ThreadSample.App/Program.cs
@@ -17,19 +17,39 @@ namespace ThreadSample
             string pipeName = Guid.NewGuid().ToString("N");
             using (CancellationTokenSource cts = new CancellationTokenSource())
             {
+                StatusInfo info = new StatusInfo();
                 Receiver receiver = new Receiver(pipeName, true);
+                receiver.DataReceived += (o, e) => info.OnReceived();
                 Sender sender = new Sender(pipeName, true);
+                sender.DataSent += (o, e) => info.OnSent();
+
+                Task statusTask = PrintStatusAsync(info, cts.Token);
 
                 Task receiveTask = receiver.ReceiveAsync(cts.Token);
                 Thread.Sleep(1000);
 
                 Task sendTask = sender.SendAsync(cts.Token);
 
-                Thread.Sleep(2000);
+                Thread.Sleep(5000);
 
                 cts.Cancel();
 
-                Task.WaitAll(receiveTask, sendTask);
+                Task.WaitAll(receiveTask, sendTask, statusTask);
+            }
+        }
+
+        private static async Task PrintStatusAsync(StatusInfo info, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Console.WriteLine("Sent: {0} / Received: {1}", info.BytesSent, info.BytesReceived);
+                try
+                {
+                    await Task.Delay(1000, token);
+                }
+                catch (OperationCanceledException)
+                {
+                }
             }
         }
     }
---
+++ projects/ThreadSample/source/ThreadSample.App/StatusInfo.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StatusInfo.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace ThreadSample
+{
+    using System.Threading;
+
+    internal sealed class StatusInfo
+    {
+        private int bytesSent;
+        private int bytesReceived;
+
+        public StatusInfo()
+        {
+        }
+
+        public int BytesSent
+        {
+            get { return Thread.VolatileRead(ref this.bytesSent); }
+        }
+
+        public int BytesReceived
+        {
+            get { return Thread.VolatileRead(ref this.bytesSent); }
+        }
+
+        public void OnSent()
+        {
+            Interlocked.Increment(ref this.bytesSent);
+        }
+
+        public void OnReceived()
+        {
+            Interlocked.Increment(ref this.bytesReceived);
+        }
+    }
+}
