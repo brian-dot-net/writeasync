d709c5169d8815fed04f219c7d649c5a41bee3be
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-01 14:31:39 -0800

    Add basic WCF contract and host

---
+++ projects/AlertSample/source/AlertSample.App/IClient.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IClient.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AlertSample
+{
+    using System.ServiceModel;
+
+    [ServiceContract]
+    internal interface IClient
+    {
+        [OperationContract(IsOneWay = false)]
+        void Send();
+    }
+}
---
+++ projects/AlertSample/source/AlertSample.App/Receiver.cs
@@ -0,0 +1,54 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Receiver.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AlertSample
+{
+    using System;
+    using System.ServiceModel;
+    using System.Threading.Tasks;
+
+    internal sealed class Receiver
+    {
+        private readonly ServiceInstance instance;
+        private readonly ServiceHost host;
+
+        public Receiver(string name)
+        {
+            this.instance = new ServiceInstance();
+            this.host = new ServiceHost(this.instance, new Uri("net.pipe://localhost/" + name));
+            this.host.AddServiceEndpoint(typeof(IClient), new NetNamedPipeBinding(NetNamedPipeSecurityMode.None), string.Empty);
+        }
+
+        public Task OpenAsync()
+        {
+            return Task.Factory.FromAsync(
+                (c, s) => ((ServiceHost)s).BeginOpen(c, s),
+                r => ((ServiceHost)r.AsyncState).EndOpen(r),
+                this.host);
+        }
+
+        public Task CloseAsync()
+        {
+            return Task.Factory.FromAsync(
+                (c, s) => ((ServiceHost)s).BeginClose(c, s),
+                r => ((ServiceHost)r.AsyncState).EndClose(r),
+                this.host);
+        }
+
+        [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+        private sealed class ServiceInstance : IClient
+        {
+            public ServiceInstance()
+            {
+            }
+
+            public void Send()
+            {
+                // TODO
+            }
+        }
+    }
+}
