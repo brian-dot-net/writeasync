b99ee627237857788eec414b7b752bffa7b712e8
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-01 13:16:09 -0800

    Add missing file

---
+++ projects/AlertSample/source/AlertSample.App/ThresholdEventArgs.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ThresholdEventArgs.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AlertSample
+{
+    using System;
+
+    internal sealed class ThresholdEventArgs : EventArgs
+    {
+        public ThresholdEventArgs(bool isLowerBound)
+        {
+            this.IsLowerBound = isLowerBound;
+        }
+
+        public bool IsLowerBound { get; private set; }
+    }
+}
