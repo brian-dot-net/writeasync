2b43feefb8f4956f4d4ac45eecfa72b23468ffd9
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-01 14:42:07 -0800

    Add basic WCF sender

--- projects/AlertSample/source/AlertSample.App/IClient.cs
+++ projects/AlertSample/source/AlertSample.App/IClient.cs
@@ -7,11 +7,19 @@
 namespace AlertSample
 {
     using System.ServiceModel;
+    using System.Threading.Tasks;
 
     [ServiceContract]
     internal interface IClient
     {
         [OperationContract(IsOneWay = false)]
         void Send();
+    }
+
+    [ServiceContract(Name = "IClient")]
+    internal interface IClientAsync
+    {
+        [OperationContract(IsOneWay = false, AsyncPattern = true)]
+        Task SendAsync();
     }
 }
---
+++ projects/AlertSample/source/AlertSample.App/Sender.cs
@@ -0,0 +1,63 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Sender.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AlertSample
+{
+    using System;
+    using System.ServiceModel;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal sealed class Sender
+    {
+        private readonly IClientAsync client;
+
+        private volatile int sendIntervalMilliseconds;
+
+        public Sender(string receiverName)
+        {
+            NetNamedPipeBinding binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            this.client = ChannelFactory<IClientAsync>.CreateChannel(binding, new EndpointAddress("net.pipe://localhost/" + receiverName));
+        }
+
+        public TimeSpan SendInterval
+        {
+            get { return TimeSpan.FromMilliseconds(this.sendIntervalMilliseconds); }
+            set { this.sendIntervalMilliseconds = (int)value.TotalMilliseconds; }
+        }
+
+        public Task OpenAsync()
+        {
+            return Task.Factory.FromAsync(
+                (c, s) => ((IClientChannel)s).BeginOpen(c, s),
+                r => ((IClientChannel)r.AsyncState).EndOpen(r),
+                (IClientChannel)this.client);
+        }
+
+        public async Task RunAsync(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await this.client.SendAsync();
+                try
+                {
+                    await Task.Delay(this.sendIntervalMilliseconds, token);
+                }
+                catch (OperationCanceledException)
+                {
+                }
+            }
+        }
+
+        public Task CloseAsync()
+        {
+            return Task.Factory.FromAsync(
+                (c, s) => ((IClientChannel)s).BeginClose(c, s),
+                r => ((IClientChannel)r.AsyncState).EndClose(r),
+                (IClientChannel)this.client);
+        }
+    }
+}
