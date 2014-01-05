481b1b82a5bf2c79107f9f4aba08751c70e9cf55
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-04 20:03:31 -0800

    Blocking.Task Should return sync func

---
+++ projects/CleanupSample/source/CleanupSample.Core/Blocking.cs
@@ -0,0 +1,42 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Blocking.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CleanupSample
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class Blocking
+    {
+        public static Func<Task> Task<TInput>(Action<TInput> action, TInput input)
+        {
+            return new BlockingTask<TInput>(action, input).Func;
+        }
+
+        private struct BlockingTask<TInput>
+        {
+            private readonly Action<TInput> action;
+            private readonly TInput input;
+
+            public BlockingTask(Action<TInput> action, TInput input)
+            {
+                this.action = action;
+                this.input = input;
+            }
+
+            public Func<Task> Func
+            {
+                get { return this.Do; }
+            }
+
+            private Task Do()
+            {
+                this.action(this.input);
+                return System.Threading.Tasks.Task.FromResult(false);
+            }
+        }
+    }
+}
---
+++ projects/CleanupSample/test/CleanupSample.Test.Unit/BlockingTaskTest.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BlockingTaskTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CleanupSample.Test.Unit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class BlockingTaskTest
+    {
+        public BlockingTaskTest()
+        {
+        }
+
+        [Fact]
+        public void Should_return_sync_func()
+        {
+            int inputValue = -1;
+            Func<Task> func = Blocking.Task(i => inputValue = i, 333);
+
+            Task task = func();
+
+            Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+            Assert.Equal(333, inputValue);
+        }
+    }
+}
