90f8c218612d2bcf34f2fe3dc8efea8cfd845b60
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-14 01:41:48 -0800

    Add sample program

--- projects/ProcessSample/source/ProcessSample.App/Program.cs
+++ projects/ProcessSample/source/ProcessSample.App/Program.cs
@@ -7,11 +7,26 @@
 namespace ProcessSample
 {
     using System;
+    using System.Diagnostics;
+    using System.Threading;
 
     internal sealed class Program
     {
         private static void Main(string[] args)
         {
+            int exitCode;
+            DateTime exitTime;
+
+            using (Process process = Process.Start("notepad.exe"))
+            using (ProcessExitWatcher watcher = new ProcessExitWatcher(new ProcessExit(process)))
+            {
+                Console.WriteLine("Waiting for Notepad to exit...");
+                watcher.WaitForExitAsync(CancellationToken.None).Wait();
+                exitCode = watcher.Status.ExitCode;
+                exitTime = watcher.Status.ExitTime;
+            }
+
+            Console.WriteLine("Done, exited with code {0} at {1}.", exitCode, exitTime);
         }
     }
 }
