f6dec186e9bb20707c477ac79ce27a5ec225380f
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-14 20:39:09 -0800

    Use async method in main program

--- projects/ProcessSample/source/ProcessSample.App/Program.cs
+++ projects/ProcessSample/source/ProcessSample.App/Program.cs
@@ -9,19 +9,42 @@ namespace ProcessSample
     using System;
     using System.Diagnostics;
     using System.Threading;
+    using System.Threading.Tasks;
 
     internal sealed class Program
     {
         private static void Main(string[] args)
         {
+            using (CancellationTokenSource cts = new CancellationTokenSource())
+            {
+                Task task = StartAndWaitForProcessAsync(cts.Token);
+                
+                Console.WriteLine("Press ENTER to quit.");
+                Console.ReadLine();
+
+                cts.Cancel();
+                try
+                {
+                    task.Wait();
+                }
+                catch (AggregateException ae)
+                {
+                    ae.Handle(e => e is OperationCanceledException);
+                    Console.WriteLine("(Canceled.)");
+                }
+            }
+        }
+
+        private static async Task StartAndWaitForProcessAsync(CancellationToken token)
+        {
             int exitCode;
             DateTime exitTime;
 
-            using (Process process = Process.Start("notepad.exe"))
+            using (Process process = await Task.Factory.StartNew(() => Process.Start("notepad.exe")))
             using (ProcessExitWatcher watcher = new ProcessExitWatcher(new ProcessExit(process)))
             {
                 Console.WriteLine("Waiting for Notepad to exit...");
-                watcher.WaitForExitAsync(CancellationToken.None).Wait();
+                await watcher.WaitForExitAsync(token);
                 exitCode = watcher.Status.ExitCode;
                 exitTime = watcher.Status.ExitTime;
             }
