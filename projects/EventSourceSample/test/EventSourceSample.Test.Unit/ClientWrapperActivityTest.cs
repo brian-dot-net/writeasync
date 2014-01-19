01db3a6e4ef3be2398cc70b0fc621e32b2592130
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-19 14:12:51 -0800

    CalculatorClientWithActivity - SquareRoot with events calls inner

--- projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientWrapperActivityTest.cs
+++ projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientWrapperActivityTest.cs
@@ -37,6 +37,17 @@ namespace EventSourceSample.Test.Unit
             clientStub.VerifyOperation("Subtract", 5.0d, 6.0d);
         }
 
+        [Fact]
+        public void SquareRoot_with_events_calls_inner()
+        {
+            CalculatorClientStub clientStub = new CalculatorClientStub();
+            clientStub.Results.Enqueue(8.0d);
+            CalculatorClientWithActivity client = new CalculatorClientWithActivity(clientStub);
+
+            VerifyResult(8.0d, client.SquareRootAsync(7.0d));
+            clientStub.VerifyOperation("SquareRoot", 7.0d, 0.0d);
+        }
+
         private static void VerifyResult(double expectedResult, Task<double> task)
         {
             Assert.Equal(TaskStatus.RanToCompletion, task.Status);
