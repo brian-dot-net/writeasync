8e7541215711b6515ad39e7aa1853c3bd8a54827
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-19 14:10:59 -0800

    CalculatorClientWithActivity - Add with events calls inner

---
+++ projects/EventSourceSample/source/EventSourceSample.Core/CalculatorClientWithActivity.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CalculatorClientWithActivity.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace EventSourceSample
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class CalculatorClientWithActivity : ICalculatorClientAsync
+    {
+        private readonly ICalculatorClientAsync inner;
+
+        public CalculatorClientWithActivity(ICalculatorClientAsync inner)
+        {
+            this.inner = inner;
+        }
+
+        public Task<double> AddAsync(double x, double y)
+        {
+            return this.inner.AddAsync(x, y);
+        }
+
+        public Task<double> SubtractAsync(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<double> SquareRootAsync(double x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientWrapperActivityTest.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ClientWrapperActivityTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace EventSourceSample.Test.Unit
+{
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class ClientWrapperActivityTest
+    {
+        public ClientWrapperActivityTest()
+        {
+        }
+
+        [Fact]
+        public void Add_with_events_calls_inner()
+        {
+            CalculatorClientStub clientStub = new CalculatorClientStub();
+            clientStub.Results.Enqueue(1.0d);
+            CalculatorClientWithActivity client = new CalculatorClientWithActivity(clientStub);
+
+            VerifyResult(1.0d, client.AddAsync(2.0d, 3.0d));
+            clientStub.VerifyOperation("Add", 2.0d, 3.0d);
+        }
+
+        private static void VerifyResult(double expectedResult, Task<double> task)
+        {
+            Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+            Assert.Equal(expectedResult, task.Result);            
+        }
+    }
+}
