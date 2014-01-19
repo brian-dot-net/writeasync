3bc4297740d921e7f85abc89a77eed8bc1cad60d
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-19 13:53:48 -0800

    Refactoring - CalculatorClientStub to outer class

---
+++ projects/EventSourceSample/test/EventSourceSample.Test.Unit/CalculatorClientStub.cs
@@ -0,0 +1,65 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CalculatorClientStub.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace EventSourceSample.Test.Unit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    internal sealed class CalculatorClientStub : ICalculatorClientAsync
+    {
+        public CalculatorClientStub()
+            : this(false)
+        {
+        }
+
+        public CalculatorClientStub(bool useDefaultResult)
+        {
+            this.Operations = new List<Tuple<string, double, double>>();
+            if (!useDefaultResult)
+            {
+                this.Results = new Queue<double>();
+            }
+        }
+
+        public IList<Tuple<string, double, double>> Operations { get; private set; }
+
+        public Queue<double> Results { get; private set; }
+
+        public Task<double> AddAsync(double x, double y)
+        {
+            return this.Complete("Add", x, y);
+        }
+
+        public Task<double> SubtractAsync(double x, double y)
+        {
+            return this.Complete("Subtract", x, y);
+        }
+
+        public Task<double> SquareRootAsync(double x)
+        {
+            return this.Complete("SquareRoot", x, 0.0d);
+        }
+
+        private Task<double> Complete(string name, double x, double y)
+        {
+            this.Operations.Add(Tuple.Create(name, x, y));
+            return Task.FromResult(this.NextResult());
+        }
+
+        private double NextResult()
+        {
+            double result = 0.0d;
+            if (this.Results != null)
+            {
+                result = this.Results.Dequeue();
+            }
+
+            return result;
+        }
+    }
+}
--- projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientTest.cs
+++ projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientTest.cs
@@ -144,58 +144,5 @@ namespace EventSourceSample.Test.Unit
                 this.Events.Add(eventData);
             }
         }
-
-        private sealed class CalculatorClientStub : ICalculatorClientAsync
-        {
-            public CalculatorClientStub()
-                : this(false)
-            {
-            }
-
-            public CalculatorClientStub(bool useDefaultResult)
-            {
-                this.Operations = new List<Tuple<string, double, double>>();
-                if (!useDefaultResult)
-                {
-                    this.Results = new Queue<double>();
-                }
-            }
-
-            public IList<Tuple<string, double, double>> Operations { get; private set; }
-
-            public Queue<double> Results { get; private set; }
-
-            public Task<double> AddAsync(double x, double y)
-            {
-                return this.Complete("Add", x, y);
-            }
-
-            public Task<double> SubtractAsync(double x, double y)
-            {
-                return this.Complete("Subtract", x, y);
-            }
-
-            public Task<double> SquareRootAsync(double x)
-            {
-                return this.Complete("SquareRoot", x, 0.0d);
-            }
-
-            private Task<double> Complete(string name, double x, double y)
-            {
-                this.Operations.Add(Tuple.Create(name, x, y));
-                return Task.FromResult(this.NextResult());
-            }
-
-            private double NextResult()
-            {
-                double result = 0.0d;
-                if (this.Results != null)
-                {
-                    result = this.Results.Dequeue();
-                }
-
-                return result;
-            }
-        }
     }
 }
