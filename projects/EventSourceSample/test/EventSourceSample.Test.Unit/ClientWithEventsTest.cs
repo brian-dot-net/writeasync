d1d6ae62a44b3557d553bd8d3ac445a434577ecb
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-19 13:57:53 -0800

    Refactoring - split events and wrapper tests

--- projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientWithEventsTest.cs
+++ projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientWithEventsTest.cs
@@ -1,33 +1,21 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="ClientTest.cs" company="Brian Rogers">
+// <copyright file="ClientWithEventsTest.cs" company="Brian Rogers">
 // Copyright (c) Brian Rogers. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
 
 namespace EventSourceSample.Test.Unit
 {
-    using System;
     using System.Collections.Generic;
     using System.Diagnostics.Tracing;
     using System.Linq;
     using System.Threading.Tasks;
     using Xunit;
 
-    public class ClientTest
+    public class ClientWithEventsTest
     {
-        public ClientTest()
+        public ClientWithEventsTest()
         {
-        }
-
-        [Fact]
-        public void Add_with_events_calls_inner()
-        {
-            CalculatorClientStub clientStub = new CalculatorClientStub();
-            clientStub.Results.Enqueue(1.0d);
-            CalculatorClientWithEvents client = new CalculatorClientWithEvents(clientStub, ClientEventSource.Instance);
-
-            VerifyResult(1.0d, client.AddAsync(2.0d, 3.0d));
-            VerifyOperation("Add", 2.0d, 3.0d, clientStub);
         }
 
         [Fact]
@@ -44,17 +32,6 @@ namespace EventSourceSample.Test.Unit
         }
 
         [Fact]
-        public void Subtract_with_events_calls_inner()
-        {
-            CalculatorClientStub clientStub = new CalculatorClientStub();
-            clientStub.Results.Enqueue(4.0d);
-            CalculatorClientWithEvents client = new CalculatorClientWithEvents(clientStub, ClientEventSource.Instance);
-
-            VerifyResult(4.0d, client.SubtractAsync(5.0d, 6.0d));
-            VerifyOperation("Subtract", 5.0d, 6.0d, clientStub);
-        }
-
-        [Fact]
         public void Subtract_with_events_traces_event()
         {
             ClientEventSource eventSource = ClientEventSource.Instance;
@@ -65,17 +42,6 @@ namespace EventSourceSample.Test.Unit
                 VerifyResult(0.0d, client.SubtractAsync(3.0d, 4.0d));
                 VerifyEvent(listener, ClientEventId.Subtract, EventLevel.Informational, ClientEventSource.Keywords.Basic, 3.0d, 4.0d);
             }
-        }
-
-        [Fact]
-        public void SquareRoot_with_events_calls_inner()
-        {
-            CalculatorClientStub clientStub = new CalculatorClientStub();
-            clientStub.Results.Enqueue(8.0d);
-            CalculatorClientWithEvents client = new CalculatorClientWithEvents(clientStub, ClientEventSource.Instance);
-
-            VerifyResult(8.0d, client.SquareRootAsync(7.0d));
-            VerifyOperation("SquareRoot", 7.0d, 0.0d, clientStub);
         }
 
         [Fact]
@@ -95,14 +61,6 @@ namespace EventSourceSample.Test.Unit
         {
             Assert.Equal(TaskStatus.RanToCompletion, task.Status);
             Assert.Equal(expectedResult, task.Result);            
-        }
-
-        private static void VerifyOperation(string expectedName, double expectedX, double expectedY, CalculatorClientStub clientStub)
-        {
-            Assert.Equal(1, clientStub.Operations.Count);
-            Assert.Equal(expectedName, clientStub.Operations[0].Item1);
-            Assert.Equal(expectedX, clientStub.Operations[0].Item2);
-            Assert.Equal(expectedY, clientStub.Operations[0].Item3);
         }
 
         private static void VerifyEvent(ClientEventListener listener, ClientEventId id, EventLevel level, EventKeywords keywords, params object[] payloadItems)
---
+++ projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientWrapperTest.cs
@@ -0,0 +1,65 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ClientWrapperTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace EventSourceSample.Test.Unit
+{
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class ClientWrapperTest
+    {
+        public ClientWrapperTest()
+        {
+        }
+
+        [Fact]
+        public void Add_with_events_calls_inner()
+        {
+            CalculatorClientStub clientStub = new CalculatorClientStub();
+            clientStub.Results.Enqueue(1.0d);
+            CalculatorClientWithEvents client = new CalculatorClientWithEvents(clientStub, ClientEventSource.Instance);
+
+            VerifyResult(1.0d, client.AddAsync(2.0d, 3.0d));
+            VerifyOperation("Add", 2.0d, 3.0d, clientStub);
+        }
+
+        [Fact]
+        public void Subtract_with_events_calls_inner()
+        {
+            CalculatorClientStub clientStub = new CalculatorClientStub();
+            clientStub.Results.Enqueue(4.0d);
+            CalculatorClientWithEvents client = new CalculatorClientWithEvents(clientStub, ClientEventSource.Instance);
+
+            VerifyResult(4.0d, client.SubtractAsync(5.0d, 6.0d));
+            VerifyOperation("Subtract", 5.0d, 6.0d, clientStub);
+        }
+
+        [Fact]
+        public void SquareRoot_with_events_calls_inner()
+        {
+            CalculatorClientStub clientStub = new CalculatorClientStub();
+            clientStub.Results.Enqueue(8.0d);
+            CalculatorClientWithEvents client = new CalculatorClientWithEvents(clientStub, ClientEventSource.Instance);
+
+            VerifyResult(8.0d, client.SquareRootAsync(7.0d));
+            VerifyOperation("SquareRoot", 7.0d, 0.0d, clientStub);
+        }
+
+        private static void VerifyResult(double expectedResult, Task<double> task)
+        {
+            Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+            Assert.Equal(expectedResult, task.Result);            
+        }
+
+        private static void VerifyOperation(string expectedName, double expectedX, double expectedY, CalculatorClientStub clientStub)
+        {
+            Assert.Equal(1, clientStub.Operations.Count);
+            Assert.Equal(expectedName, clientStub.Operations[0].Item1);
+            Assert.Equal(expectedX, clientStub.Operations[0].Item2);
+            Assert.Equal(expectedY, clientStub.Operations[0].Item3);
+        }
+    }
+}
