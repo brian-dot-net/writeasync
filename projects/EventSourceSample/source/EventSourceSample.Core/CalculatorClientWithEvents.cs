d7bc6ee6a7f1228534d57eb45d5793e6b63dd529
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-19 12:27:11 -0800

    Add with events calls inner

---
+++ projects/EventSourceSample/source/EventSourceSample.Core/CalculatorClientWithEvents.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CalculatorClientWithEvents.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace EventSourceSample
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class CalculatorClientWithEvents : ICalculatorClientAsync
+    {
+        private readonly ICalculatorClientAsync inner;
+
+        public CalculatorClientWithEvents(ICalculatorClientAsync inner)
+        {
+            this.inner = inner;
+        }
+
+        public Task<double> AddAsync(double x, double y)
+        {
+            return this.inner.AddAsync(x, y);
+        }
+
+        public Task<double> SubtractAsync(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<double> SquareRootAsync(double x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientTest.cs
+++ projects/EventSourceSample/test/EventSourceSample.Test.Unit/ClientTest.cs
@@ -6,6 +6,8 @@
 
 namespace EventSourceSample.Test.Unit
 {
+    using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Xunit;
 
@@ -16,8 +18,55 @@ namespace EventSourceSample.Test.Unit
         }
 
         [Fact]
-        public void Todo()
+        public void Add_with_events_calls_inner()
         {
+            CalculatorClientStub clientStub = new CalculatorClientStub();
+            clientStub.Results.Enqueue(1.0d);
+            CalculatorClientWithEvents client = new CalculatorClientWithEvents(clientStub);
+
+            Task<double> task = client.AddAsync(2.0d, 3.0d);
+
+            Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+            Assert.Equal(1.0d, task.Result);
+
+            Assert.Equal(1, clientStub.Operations.Count);
+            Assert.Equal("Add", clientStub.Operations[0].Item1);
+            Assert.Equal(2.0d, clientStub.Operations[0].Item2);
+            Assert.Equal(3.0d, clientStub.Operations[0].Item3);
+        }
+
+        private sealed class CalculatorClientStub : ICalculatorClientAsync
+        {
+            public CalculatorClientStub()
+            {
+                this.Operations = new List<Tuple<string, double, double>>();
+                this.Results = new Queue<double>();
+            }
+
+            public IList<Tuple<string, double, double>> Operations { get; private set; }
+
+            public Queue<double> Results { get; private set; }
+
+            public Task<double> AddAsync(double x, double y)
+            {
+                return this.Complete("Add", x, y);
+            }
+
+            public Task<double> SubtractAsync(double x, double y)
+            {
+                return this.Complete("Subtract", x, y);
+            }
+
+            public Task<double> SquareRootAsync(double x)
+            {
+                return this.Complete("SquareRoot", x, 0.0d);
+            }
+
+            private Task<double> Complete(string name, double x, double y)
+            {
+                this.Operations.Add(Tuple.Create(name, x, y));
+                return Task.FromResult(this.Results.Dequeue());
+            }
         }
     }
 }
