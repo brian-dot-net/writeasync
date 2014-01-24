abe205d9070fc8bd036a650795fa1858b8ac1c16
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-23 21:08:40 -0800

    Add adds operation to pending

---
+++ projects/TraceAnalysisSample/source/TraceAnalysisSample.Core/EventWindow.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EventWindow.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TraceAnalysisSample
+{
+    using System;
+
+    public class EventWindow
+    {
+        private int pendingCount;
+
+        public EventWindow()
+        {
+        }
+
+        public void Add(int eventId, Guid instanceId)
+        {
+            ++this.pendingCount;
+        }
+
+        public int GetPendingCount(int eventId)
+        {
+            return this.pendingCount;
+        }
+    }
+}
---
+++ projects/TraceAnalysisSample/test/TraceAnalysisSample.Test.Unit/EventWindowTest.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EventWindowTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TraceAnalysisSample.Test.Unit
+{
+    using System;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class EventWindowTest
+    {
+        public EventWindowTest()
+        {
+        }
+
+        [Fact]
+        public void Add_adds_operation_to_pending()
+        {
+            EventWindow window = new EventWindow();
+            int eventId = 1;
+            Guid instanceId = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+
+            window.Add(eventId, instanceId);
+
+            Assert.Equal(1, window.GetPendingCount(eventId));
+        }
+    }
+}
