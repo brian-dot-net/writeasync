078f7a17da9741acfbd5df1383e5157df9f74732
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-24 00:41:09 -0800

    One start within window produces one after close

--- projects/TraceAnalysisSample/test/TraceAnalysisSample.Test.Unit/EventCollectorTest.cs
+++ projects/TraceAnalysisSample/test/TraceAnalysisSample.Test.Unit/EventCollectorTest.cs
@@ -17,6 +17,28 @@ namespace TraceAnalysisSample.Test.Unit
         }
 
         [Fact]
+        public void One_start_within_window_produces_one_after_close()
+        {
+            EventCollector collector = new EventCollector();
+            int eventId = 1;
+            Guid instanceId = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+            DateTime startTime = new DateTime(2000, 1, 2, 3, 4, 5, 6);
+            List<EventWindow> windows = new List<EventWindow>();
+
+            collector.WindowClosed += (o, e) => windows.Add(e.Window);
+
+            collector.OnStart(eventId, instanceId, startTime);
+
+            Assert.Equal(0, windows.Count);
+
+            collector.CloseWindow();
+
+            Assert.Equal(1, windows.Count);
+            Assert.Equal(startTime, windows[0].StartTime);
+            Assert.Equal(1, windows[0].GetPendingCount(eventId));
+        }
+
+        [Fact]
         public void One_start_end_pair_within_window_produces_one_after_close()
         {
             EventCollector collector = new EventCollector();
