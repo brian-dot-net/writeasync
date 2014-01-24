e714637439ffdcf771aa199698146992e3a03068
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-23 21:13:28 -0800

    Add many types adds separate operations to pending

--- projects/TraceAnalysisSample/test/TraceAnalysisSample.Test.Unit/EventWindowTest.cs
+++ projects/TraceAnalysisSample/test/TraceAnalysisSample.Test.Unit/EventWindowTest.cs
@@ -57,5 +57,31 @@ namespace TraceAnalysisSample.Test.Unit
             Assert.Equal(1, window.GetPendingCount(eventId1));
             Assert.Equal(1, window.GetPendingCount(eventId2));
         }
+
+        [Fact]
+        public void Add_many_types_adds_separate_operations_to_pending()
+        {
+            EventWindow window = new EventWindow();
+            int eventIdA = 1;
+            Guid instanceIdA1 = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+            int eventIdB = 2;
+            Guid instanceIdB1 = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+            Guid instanceIdB2 = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3);
+            int eventIdC = 3;
+            Guid instanceIdC1 = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4);
+            Guid instanceIdC2 = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5);
+            Guid instanceIdC3 = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6);
+
+            window.Add(eventIdA, instanceIdA1);
+            window.Add(eventIdB, instanceIdB1);
+            window.Add(eventIdB, instanceIdB2);
+            window.Add(eventIdC, instanceIdC1);
+            window.Add(eventIdC, instanceIdC2);
+            window.Add(eventIdC, instanceIdC3);
+
+            Assert.Equal(1, window.GetPendingCount(eventIdA));
+            Assert.Equal(2, window.GetPendingCount(eventIdB));
+            Assert.Equal(3, window.GetPendingCount(eventIdC));
+        }
     }
 }
