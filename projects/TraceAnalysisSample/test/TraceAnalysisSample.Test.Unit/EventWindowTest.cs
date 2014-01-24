bf7cb81cc7ad40aba68fb378d86bbeb3155a8fa7
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-23 21:09:28 -0800

    Add same type twices adds 2 operations to pending

--- projects/TraceAnalysisSample/test/TraceAnalysisSample.Test.Unit/EventWindowTest.cs
+++ projects/TraceAnalysisSample/test/TraceAnalysisSample.Test.Unit/EventWindowTest.cs
@@ -27,5 +27,19 @@ namespace TraceAnalysisSample.Test.Unit
 
             Assert.Equal(1, window.GetPendingCount(eventId));
         }
+
+        [Fact]
+        public void Add_same_type_twices_adds_2_operations_to_pending()
+        {
+            EventWindow window = new EventWindow();
+            int eventId = 1;
+            Guid instanceId1 = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+            Guid instanceId2 = new Guid(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2);
+
+            window.Add(eventId, instanceId1);
+            window.Add(eventId, instanceId2);
+
+            Assert.Equal(2, window.GetPendingCount(eventId));
+        }
     }
 }
