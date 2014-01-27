11ce6b5fc0b3f6b0f0e38cd24385a953191d038c
Brian Rogers <bobbymcr@bobbymcr.com>
2014-01-26 21:40:18 -0800

    Add QueueEventSource / Id

---
+++ projects/QueueSample/source/QueueSample.Core/QueueEventId.cs
@@ -0,0 +1,13 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="QueueEventId.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace QueueSample
+{
+    public enum QueueEventId : ushort
+    {
+        None = 0,
+    }
+}
---
+++ projects/QueueSample/source/QueueSample.Core/QueueEventSource.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="QueueEventSource.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace QueueSample
+{
+    using System;
+    using System.Diagnostics.Tracing;
+
+    [EventSource(Guid = ProviderIdString)]
+    public class QueueEventSource : EventSource
+    {
+        public const string ProviderIdString = "{B1E1772F-11D9-4986-87BA-80A2A2AE7694}";
+        public static readonly Guid ProviderId = new Guid(ProviderIdString);
+
+        private static readonly QueueEventSource SingletonInstance = new QueueEventSource();
+
+        private QueueEventSource()
+        {
+        }
+
+        public static QueueEventSource Instance
+        {
+            get { return SingletonInstance; }
+        }
+    }
+}
