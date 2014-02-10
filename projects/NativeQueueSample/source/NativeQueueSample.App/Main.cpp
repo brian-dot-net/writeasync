c066820220ec70901885a71df3133b9bdc9e8b90
Brian Rogers <bobbymcr@bobbymcr.com>
2014-02-09 23:41:06 -0800

    Changes for main.cpp

--- projects/NativeQueueSample/source/NativeQueueSample.App/Main.cpp
+++ projects/NativeQueueSample/source/NativeQueueSample.App/Main.cpp
@@ -6,11 +6,36 @@
 
 #include <iostream>
 #include "InputQueue.h"
+#include "Sample.h"
 
+using namespace concurrency;
+using namespace concurrency::extras;
 using namespace NativeQueueSample;
 using namespace std;
 
 int wmain(int argc, wchar_t ** argv)
 {
-    return 0;
+    vector<task<void>> tasks;
+    cancellation_token_source cts;
+    {
+        InputQueue<int> queue;
+        tasks.push_back(PrintException(EnqueueLoopAsync(queue, cts.get_token())));
+        tasks.push_back(PrintException(DequeueLoopAsync(queue, cts.get_token())));
+
+        wcout << L"Press ENTER to cancel." << endl;
+        wstring line;
+        getline(wcin, line);
+
+        cts.cancel();
+    }
+
+    wcout << L"Waiting for tasks..." << endl;
+    try
+    {
+        when_all(tasks.begin(), tasks.end()).get();
+    }
+    catch (exception & e)
+    {
+        cout << "!! FAILED !! -- " << e.what() << endl;
+    }
 }
