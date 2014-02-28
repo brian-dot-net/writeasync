9f89466108f552fbc877ec6ba30256c3f17e52ec
Brian Rogers <bobbymcr@bobbymcr.com>
2014-02-27 22:19:07 -0800

    Add ElapsedTimer

---
+++ projects/RetrySample/source/RetrySample.Core/ElapsedTimer.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ElapsedTimer.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace RetrySample
+{
+    using System;
+    using System.Diagnostics;
+
+    public sealed class ElapsedTimer : IElapsedTimer
+    {
+        private readonly Stopwatch stopwatch;
+
+        public ElapsedTimer()
+        {
+            this.stopwatch = Stopwatch.StartNew();
+        }
+
+        public TimeSpan Elapsed
+        {
+            get { return this.stopwatch.Elapsed; }
+        }
+    }
+}
