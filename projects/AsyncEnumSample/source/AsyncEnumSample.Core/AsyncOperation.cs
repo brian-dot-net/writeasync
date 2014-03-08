85ad09ab8e118e04a371a4d623ba7adde80b0b0d
Brian Rogers <bobbymcr@bobbymcr.com>
2014-03-08 12:26:55 -0800

    Set result in ctor and break completes sync

--- projects/AsyncEnumSample/source/AsyncEnumSample.Core/AsyncOperation.cs
+++ projects/AsyncEnumSample/source/AsyncEnumSample.Core/AsyncOperation.cs
@@ -6,9 +6,36 @@
 
 namespace AsyncEnumSample
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
     public abstract class AsyncOperation<TResult>
     {
+        private TaskCompletionSource<TResult> tcs;
+
         protected AsyncOperation()
+        {
+        }
+
+        protected TResult Result { get; set; }
+
+        public Task<TResult> Start()
+        {
+            this.tcs = new TaskCompletionSource<TResult>();
+            IEnumerator<Step> steps = this.Steps();
+            while (steps.MoveNext())
+            {
+                throw new NotImplementedException();
+            }
+
+            this.tcs.SetResult(this.Result);
+            return this.tcs.Task;
+        }
+
+        protected abstract IEnumerator<Step> Steps();
+
+        protected struct Step
         {
         }
     }
--- projects/AsyncEnumSample/test/AsyncEnumSample.Test.Unit/AsyncOperationTest.cs
+++ projects/AsyncEnumSample/test/AsyncEnumSample.Test.Unit/AsyncOperationTest.cs
@@ -6,6 +6,8 @@
 
 namespace AsyncEnumSample.Test.Unit
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
     using Xunit;
 
     public class AsyncOperationTest
@@ -15,8 +17,26 @@ namespace AsyncEnumSample.Test.Unit
         }
 
         [Fact]
-        public void Todo()
+        public void Set_result_in_ctor_and_break_completes_sync()
         {
+            SetResultInCtorOperation op = new SetResultInCtorOperation(1234);
+            Task<int> task = op.Start();
+
+            Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+            Assert.Equal(1234, task.Result);
+        }
+
+        private sealed class SetResultInCtorOperation : AsyncOperation<int>
+        {
+            public SetResultInCtorOperation(int result)
+            {
+                this.Result = result;
+            }
+
+            protected override IEnumerator<Step> Steps()
+            {
+                yield break;
+            }
         }
     }
 }
