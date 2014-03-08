5d5e3bfabe3f999c5631bba6206087f11ba4cc7c
Brian Rogers <bobbymcr@bobbymcr.com>
2014-03-08 12:28:35 -0800

    Set result in enumerator and break completes sync

--- projects/AsyncEnumSample/test/AsyncEnumSample.Test.Unit/AsyncOperationTest.cs
+++ projects/AsyncEnumSample/test/AsyncEnumSample.Test.Unit/AsyncOperationTest.cs
@@ -26,6 +26,16 @@ namespace AsyncEnumSample.Test.Unit
             Assert.Equal(1234, task.Result);
         }
 
+        [Fact]
+        public void Set_result_in_enumerator_and_break_completes_sync()
+        {
+            SetResultInEnumeratorOperation op = new SetResultInEnumeratorOperation(1234);
+            Task<int> task = op.Start();
+
+            Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+            Assert.Equal(1234, task.Result);
+        }
+
         private sealed class SetResultInCtorOperation : AsyncOperation<int>
         {
             public SetResultInCtorOperation(int result)
@@ -35,6 +45,22 @@ namespace AsyncEnumSample.Test.Unit
 
             protected override IEnumerator<Step> Steps()
             {
+                yield break;
+            }
+        }
+
+        private sealed class SetResultInEnumeratorOperation : AsyncOperation<int>
+        {
+            private readonly int result;
+
+            public SetResultInEnumeratorOperation(int result)
+            {
+                this.result = result;
+            }
+
+            protected override IEnumerator<Step> Steps()
+            {
+                this.Result = this.result;
                 yield break;
             }
         }
