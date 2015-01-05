279e85c79dd217227db4e64971dd3e91bf36f76d
Brian Rogers <bobbymcr@bobbymcr.com>
2015-01-05 11:17:23 -0800

    Should allow chaining with Which (BePendingTest)

--- projects/FluentSample/test/FluentSample.Test.Unit/BePendingTest.cs
+++ projects/FluentSample/test/FluentSample.Test.Unit/BePendingTest.cs
@@ -124,5 +124,15 @@ namespace FluentSample.Test.Unit
 
             act.ShouldNotThrow();
         }
+
+        [TestMethod]
+        public void ShouldAllowChainingWithWhich()
+        {
+            Task task = TaskBuilder.Pending();
+
+            Action act = () => task.Should().BePending().Which.IsCompleted.Should().BeFalse();
+
+            act.ShouldNotThrow();
+        }
     }
 }
