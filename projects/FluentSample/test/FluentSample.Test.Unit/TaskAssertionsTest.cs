b47562057dfa33e3b4a6f4c859dd96306748217c
Brian Rogers <bobbymcr@bobbymcr.com>
2015-01-05 09:33:55 -0800

    Add TaskAssertionsTest skeleton

---
+++ projects/FluentSample/test/FluentSample.Test.Unit/TaskAssertionsTest.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TaskAssertionsTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace FluentSample.Test.Unit
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TaskAssertionsTest
+    {
+        [TestMethod]
+        public void Todo()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
