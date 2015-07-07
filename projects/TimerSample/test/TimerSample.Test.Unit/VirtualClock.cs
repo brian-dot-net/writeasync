c22bbfe600ff1364efc4751adf8cf252e640c8bf
Brian Rogers <bobbymcr@bobbymcr.com>
2015-07-07 08:45:07 -0700

    VirtualClock should invoke created action on Sleep endpoint of interval

---
+++ projects/TimerSample/test/TimerSample.Test.Unit/VirtualClock.cs
@@ -0,0 +1,50 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="VirtualClock.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TimerSample.Test.Unit
+{
+    using System;
+
+    internal sealed class VirtualClock
+    {
+        private VirtualAction action;
+
+        public VirtualClock()
+        {
+        }
+
+        public PeriodicAction CreateAction(TimeSpan interval, Action action)
+        {
+            return this.action = new VirtualAction(interval, action);
+        }
+
+        public void Sleep(TimeSpan interval)
+        {
+            this.action.Invoke();
+        }
+
+        private sealed class VirtualAction : PeriodicAction
+        {
+            private readonly Action action;
+
+            public VirtualAction(TimeSpan interval, Action action)
+                : base(interval, action)
+            {
+                this.action = action;
+            }
+
+            public void Invoke()
+            {
+                this.action();
+            }
+
+            public override void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ projects/TimerSample/test/TimerSample.Test.Unit/VirtualClockTest.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="VirtualClockTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TimerSample.Test.Unit
+{
+    using System;
+    using System.Threading;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class VirtualClockTest
+    {
+        [TestMethod]
+        public void ShouldInvokeCreatedActionOnSleepEndpointOfInterval()
+        {
+            VirtualClock clock = new VirtualClock();
+            int invokeCount = 0;
+            TimeSpan interval = TimeSpan.FromSeconds(1.0d);
+
+            clock.CreateAction(interval, () => ++invokeCount);
+
+            Assert.AreEqual(0, invokeCount);
+
+            clock.Sleep(interval);
+
+            Assert.AreEqual(1, invokeCount);
+
+            clock.Sleep(interval);
+
+            Assert.AreEqual(2, invokeCount);
+        }
+    }
+}
