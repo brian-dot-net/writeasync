5ab501fd86ef0596814177effe6bb2f0be89cbc7
Brian Rogers <bobbymcr@bobbymcr.com>
2015-07-07 06:52:02 -0700

    Should schedule action on interval after Add

---
+++ projects/TimerSample/source/TimerSample.Core/TimerGroup.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TimerGroup.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TimerSample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public class TimerGroup
+    {
+        private readonly List<Timer> timers;
+
+        public TimerGroup()
+        {
+            this.timers = new List<Timer>();
+        }
+
+        public void Add(TimeSpan interval, Action action)
+        {
+            this.timers.Add(new Timer(o => action(), null, interval, interval));
+        }
+    }
+}
---
+++ projects/TimerSample/test/TimerSample.Test.Unit/TimerGroupTest.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TimerGroupTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TimerSample.Test.Unit
+{
+    using System;
+    using System.Threading;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TimerGroupTest
+    {
+        [TestMethod]
+        public void ShouldScheduleActionOnIntervalAfterAdd()
+        {
+            TimerGroup timers = new TimerGroup();
+
+            int invokeCount = 0;
+            timers.Add(TimeSpan.FromSeconds(1.0d), () => ++invokeCount);
+
+            Assert.AreEqual(0, invokeCount);
+
+            Thread.Sleep(TimeSpan.FromSeconds(1.1d));
+
+            Assert.AreEqual(1, invokeCount);
+        }
+    }
+}
