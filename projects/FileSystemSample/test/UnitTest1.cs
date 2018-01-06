abb2c1805fe0ffbe865d47eda78240738e432a8f
Brian Rogers <bobbymcr@gmail.com>
2018-01-06 12:27:03 -0800

    Add FileSystemSample stub

---
+++ projects/FileSystemSample/test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="UnitTest1.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace FileSystemSample.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
