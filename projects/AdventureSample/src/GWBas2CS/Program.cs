d66de47ed80984f3cb9fcdfbf841975251259f54
Brian Rogers <bobbymcr@gmail.com>
2018-03-02 18:10:06 -0800

    AdventureSample - convert program containing 1 comment

--- projects/AdventureSample/src/GWBas2CS/Program.cs
+++ projects/AdventureSample/src/GWBas2CS/Program.cs
@@ -4,13 +4,23 @@
 
 namespace GWBas2CS
 {
-    using System;
+    using System.IO;
+    using System.Threading.Tasks;
 
     internal sealed class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(new Sample("x"));
+            MainAsync(args).Wait();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            using (SourceCodeStream input = new SourceCodeStream(new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true)))
+            using (Stream output = new FileStream(args[1], FileMode.Create, FileAccess.Write, FileShare.None, 4096, true))
+            {
+                await input.TranslateAsync(Path.GetFileNameWithoutExtension(args[0]), output);
+            }
         }
     }
 }
---
+++ projects/AdventureSample/test/GWBas2CS.Test/ExampleProgram.cs
@@ -0,0 +1,76 @@
+// <copyright file="ExampleProgram.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace GWBas2CS.Test
+{
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ExampleProgram
+    {
+        [Fact]
+        public void WithOneCommentLine()
+        {
+            const string Input = "10 REM A comment";
+            const string Expected = @"using System;
+
+internal sealed class MyProg
+{
+    public void Run()
+    {
+        while (this.RunCore())
+        {
+        }
+    }
+
+    private bool RunCore()
+    {
+        // A comment
+        return false;
+    }
+}";
+
+            string actual = Translate("MyProg", Input);
+
+            actual.Should().Be(Expected);
+        }
+
+        private static string Translate(string name, string inputCode)
+        {
+            WrappedMemoryStream stream = new WrappedMemoryStream(Encoding.UTF8.GetBytes(inputCode));
+            string outputCode;
+            using (SourceCodeStream source = new SourceCodeStream(stream))
+            using (MemoryStream output = new MemoryStream())
+            {
+                Task task = source.TranslateAsync(name, output);
+
+                task.Exception.Should().BeNull();
+                task.IsCompletedSuccessfully.Should().BeTrue();
+                outputCode = Encoding.UTF8.GetString(output.ToArray());
+            }
+
+            stream.DisposeCount.Should().Be(1);
+            return outputCode;
+        }
+
+        private sealed class WrappedMemoryStream : MemoryStream
+        {
+            public WrappedMemoryStream(byte[] buffer)
+                : base(buffer)
+            {
+            }
+
+            public int DisposeCount { get; private set; }
+
+            protected override void Dispose(bool disposing)
+            {
+                ++this.DisposeCount;
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
