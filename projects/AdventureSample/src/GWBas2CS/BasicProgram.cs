5bcc656f6ff996930349733d02df7fc98f8a9e1b
Brian Rogers <bobbymcr@gmail.com>
2018-03-02 18:59:36 -0800

    AdventureSample - extract BasicProgram from SourceCodeStream

---
+++ projects/AdventureSample/src/GWBas2CS/BasicProgram.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="BasicProgram.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace GWBas2CS
+{
+    using System.Collections.Generic;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Editing;
+
+    internal sealed class BasicProgram
+    {
+        private readonly string name;
+
+        private SyntaxTrivia commentNode;
+
+        public BasicProgram(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddComment(string comment)
+        {
+            this.commentNode = SyntaxFactory.Comment("// " + comment);
+        }
+
+        public override string ToString()
+        {
+            var workspace = new AdhocWorkspace();
+            var generator = SyntaxGenerator.GetGenerator(workspace, LanguageNames.CSharp);
+            var usingDirectives = generator.NamespaceImportDeclaration("System");
+
+            List<SyntaxNode> classMembers = new List<SyntaxNode>();
+
+            var runCoreMember = generator.MemberAccessExpression(generator.ThisExpression(), "RunCore");
+            var callRunCore = generator.InvocationExpression(runCoreMember);
+            List<SyntaxNode> runStatements = new List<SyntaxNode>();
+            var whileLoop = generator.WhileStatement(callRunCore, null);
+            runStatements.Add(whileLoop);
+            var runMethod = generator.MethodDeclaration("Run", accessibility: Accessibility.Public, statements: runStatements);
+
+            classMembers.Add(runMethod);
+
+            List<SyntaxNode> runCoreStatements = new List<SyntaxNode>();
+            var lastStatement = generator.ReturnStatement(generator.LiteralExpression(false));
+            runCoreStatements.Add(lastStatement.WithLeadingTrivia(this.commentNode));
+            var boolType = generator.TypeExpression(SpecialType.System_Boolean);
+            var runCoreMethod = generator.MethodDeclaration("RunCore", accessibility: Accessibility.Private, returnType: boolType, statements: runCoreStatements);
+
+            classMembers.Add(runCoreMethod);
+
+            var classDecl = generator.ClassDeclaration(this.name, accessibility: Accessibility.Internal, modifiers: DeclarationModifiers.Sealed, members: classMembers);
+            return generator.CompilationUnit(usingDirectives, classDecl).NormalizeWhitespace().ToString();
+        }
+    }
+}
--- projects/AdventureSample/src/GWBas2CS/SourceCodeStream.cs
+++ projects/AdventureSample/src/GWBas2CS/SourceCodeStream.cs
@@ -5,13 +5,9 @@
 namespace GWBas2CS
 {
     using System;
-    using System.Collections.Generic;
     using System.IO;
     using System.Text;
     using System.Threading.Tasks;
-    using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp;
-    using Microsoft.CodeAnalysis.Editing;
 
     public sealed class SourceCodeStream : IDisposable
     {
@@ -24,36 +20,17 @@ namespace GWBas2CS
 
         public async Task TranslateAsync(string name, Stream output)
         {
+            BasicProgram program = new BasicProgram(name);
+
             string line = await this.reader.ReadLineAsync();
             string[] numberAndStatement = line.Split(new char[] { ' ' }, 2);
             string[] keywordAndRest = numberAndStatement[1].Split(new char[] { ' ' }, 2);
             string comment = keywordAndRest[1];
 
-            var workspace = new AdhocWorkspace();
-            var generator = SyntaxGenerator.GetGenerator(workspace, LanguageNames.CSharp);
-            var usingDirectives = generator.NamespaceImportDeclaration("System");
+            program.AddComment(comment);
 
-            List<SyntaxNode> classMembers = new List<SyntaxNode>();
+            string outputCode = program.ToString();
 
-            var runCoreMember = generator.MemberAccessExpression(generator.ThisExpression(), "RunCore");
-            var callRunCore = generator.InvocationExpression(runCoreMember);
-            List<SyntaxNode> runStatements = new List<SyntaxNode>();
-            var whileLoop = generator.WhileStatement(callRunCore, null);
-            runStatements.Add(whileLoop);
-            var runMethod = generator.MethodDeclaration("Run", accessibility: Accessibility.Public, statements: runStatements);
-
-            classMembers.Add(runMethod);
-
-            List<SyntaxNode> runCoreStatements = new List<SyntaxNode>();
-            var commentNode = SyntaxFactory.Comment("// " + comment);
-            runCoreStatements.Add(generator.ReturnStatement(generator.LiteralExpression(false)).WithLeadingTrivia(commentNode));
-            var boolType = generator.TypeExpression(SpecialType.System_Boolean);
-            var runCoreMethod = generator.MethodDeclaration("RunCore", accessibility: Accessibility.Private, returnType: boolType, statements: runCoreStatements);
-
-            classMembers.Add(runCoreMethod);
-
-            var classDecl = generator.ClassDeclaration(name, accessibility: Accessibility.Internal, modifiers: DeclarationModifiers.Sealed, members: classMembers);
-            var outputCode = generator.CompilationUnit(usingDirectives, classDecl).NormalizeWhitespace();
             byte[] rawOutput = Encoding.UTF8.GetBytes(outputCode.ToString());
             await output.WriteAsync(rawOutput, 0, rawOutput.Length);
         }
