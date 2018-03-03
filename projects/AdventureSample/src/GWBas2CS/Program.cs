0c62465baa2a36bb4479061536a271b448ce003d
Brian Rogers <bobbymcr@gmail.com>
2018-03-02 21:12:04 -0800

    AdventureSample - improve console app

--- projects/AdventureSample/src/GWBas2CS/Program.cs
+++ projects/AdventureSample/src/GWBas2CS/Program.cs
@@ -4,22 +4,30 @@
 
 namespace GWBas2CS
 {
+    using System;
     using System.IO;
     using System.Threading.Tasks;
 
     internal sealed class Program
     {
-        private static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            MainAsync(args).Wait();
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please provider an input (.bas) and output (.cs) file path.");
+                return 1;
+            }
+
+            MainAsync(args[0], args[1]).Wait();
+            return 0;
         }
 
-        private static async Task MainAsync(string[] args)
+        private static async Task MainAsync(string inputPath, string outputPath)
         {
-            using (SourceCodeStream input = new SourceCodeStream(new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true)))
-            using (Stream output = new FileStream(args[1], FileMode.Create, FileAccess.Write, FileShare.None, 4096, true))
+            using (SourceCodeStream input = new SourceCodeStream(new FileStream(inputPath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true)))
+            using (Stream output = new FileStream(outputPath, FileMode.Create, FileAccess.Write, FileShare.None, 4096, true))
             {
-                await input.TranslateAsync(Path.GetFileNameWithoutExtension(args[0]), output);
+                await input.TranslateAsync(Path.GetFileNameWithoutExtension(inputPath), output);
             }
         }
     }
