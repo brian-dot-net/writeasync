7a422af91e3f1028c9c3458a7e52493a2440ee22
Brian Rogers <bobbymcr@gmail.com>
2018-03-10 09:02:26 -0800

    Four4Sample - initial commit

---
+++ projects/Four4Sample/Four4.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+// <copyright file="UnitTest1.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Four4.Test
+{
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ projects/Four4Sample/Four4/Program.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Program.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Four4
+{
+    using System;
+
+    internal sealed class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
