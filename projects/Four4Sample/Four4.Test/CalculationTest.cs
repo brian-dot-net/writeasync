6c89ac17ef3039ae9647bfb81c4a05ae17ba142d
Brian Rogers <bobbymcr@gmail.com>
2018-03-11 08:22:19 -0700

    Four4Sample - more addition tests

--- projects/Four4Sample/Four4.Test/CalculationTest.cs
+++ projects/Four4Sample/Four4.Test/CalculationTest.cs
@@ -51,6 +51,8 @@ namespace Four4.Test
         [Theory]
         [InlineData("4 4 +", "8")]
         [InlineData("4 .4 +", "22/5")]
+        [InlineData(".4 .44 4 + +", "121/25")]
+        [InlineData("4 44 + 4 +", "52")]
         public void Addition(string input, string result)
         {
             Calculation.FromString(input).Should().Be(result);
