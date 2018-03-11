6bf2ee8d7112f3c89ad0f643c81e026105829adb
Brian Rogers <bobbymcr@gmail.com>
2018-03-11 08:20:09 -0700

    Four4Sample - add tests for all combinations of 4s and '.'

--- projects/Four4Sample/Four4.Test/CalculationTest.cs
+++ projects/Four4Sample/Four4.Test/CalculationTest.cs
@@ -13,11 +13,37 @@ namespace Four4.Test
         [InlineData("4", "4")]
         [InlineData(".4", "2/5")]
         [InlineData(".4_", "4/9")]
+        public void NumbersWithOne4(string input, string result)
+        {
+            Calculation.FromString(input).Should().Be(result, "input was {0}", input);
+        }
+
+        [Theory]
         [InlineData(".44", "11/25")]
-        [InlineData(".444", "111/250")]
-        [InlineData(".4444", "1111/2500")]
         [InlineData("4.4", "22/5")]
-        public void Numbers(string input, string result)
+        [InlineData("44", "44")]
+        public void NumbersWithTwo4s(string input, string result)
+        {
+            Calculation.FromString(input).Should().Be(result, "input was {0}", input);
+        }
+
+        [Theory]
+        [InlineData(".444", "111/250")]
+        [InlineData("4.44", "111/25")]
+        [InlineData("44.4", "222/5")]
+        [InlineData("444", "444")]
+        public void NumbersWithThree4s(string input, string result)
+        {
+            Calculation.FromString(input).Should().Be(result, "input was {0}", input);
+        }
+
+        [Theory]
+        [InlineData(".4444", "1111/2500")]
+        [InlineData("4.444", "1111/250")]
+        [InlineData("44.44", "1111/25")]
+        [InlineData("444.4", "2222/5")]
+        [InlineData("4444", "4444")]
+        public void NumbersWithFour4s(string input, string result)
         {
             Calculation.FromString(input).Should().Be(result, "input was {0}", input);
         }
