580f8a5c8888e2daaa1d03591446d82779ae713d
Brian Rogers <bobbymcr@gmail.com>
2018-03-11 09:46:08 -0700

    Four4Sample - too many operands (-)

--- projects/Four4Sample/Four4.Test/CalculatorTest.cs
+++ projects/Four4Sample/Four4.Test/CalculatorTest.cs
@@ -73,6 +73,7 @@ namespace Four4.Test
         [InlineData("4 4 4 - - 4 -", "0")]
         [InlineData("4 4 4 - / 4 -", "NaN")]
         [InlineData("4 -", "NaN")]
+        [InlineData("4 4 4 -", "NaN")]
         public void Subtraction(string input, string result)
         {
             TestEval(input, result);
