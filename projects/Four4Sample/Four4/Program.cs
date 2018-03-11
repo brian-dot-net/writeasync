ac96fc18cd2de0b0f72b40cfb47348ddf0de680b
Brian Rogers <bobbymcr@gmail.com>
2018-03-11 14:45:24 -0700

    Four4Sample - add Four 4's console app

--- projects/Four4Sample/Four4/Program.cs
+++ projects/Four4Sample/Four4/Program.cs
@@ -5,12 +5,80 @@
 namespace Four4
 {
     using System;
+    using System.Collections.Generic;
 
     internal sealed class Program
     {
+        private readonly ExpressionSearch search;
+        private readonly SortedDictionary<int, string> results;
+
+        public Program()
+        {
+            this.search = InitSearch();
+            this.results = new SortedDictionary<int, string>();
+        }
+
+        public void Run()
+        {
+            this.search.Run(this.OnFound);
+
+            Console.WriteLine("Found {0} results.", this.results.Count);
+
+            foreach (KeyValuePair<int, string> result in this.results)
+            {
+                Console.WriteLine("{0} = {1}", result.Key, result.Value);
+            }
+        }
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().Run();
+        }
+
+        private static ExpressionSearch InitSearch()
+        {
+            ExpressionSearch search = new ExpressionSearch();
+            search.AddBinary("+");
+            search.AddBinary("-");
+            search.AddBinary("*");
+            search.AddBinary("/");
+            search.AddUnary("!");
+            search.AddUnary("R");
+            search.AddOperand("4");
+            search.AddOperand("44");
+            search.AddOperand("444");
+            search.AddOperand("4444");
+            search.AddOperand(Number.PointFour);
+            search.AddOperand(Number.PointFourRepeating);
+            return search;
+        }
+
+        private bool OnFound(Expression expr)
+        {
+            if (expr.NumeralCount != 4)
+            {
+                return true;
+            }
+
+            Number num = expr.Result;
+            if (!num.IsWhole)
+            {
+                return true;
+            }
+
+            int n = (int)num;
+            if (n > 100)
+            {
+                return true;
+            }
+
+            if (this.results.ContainsKey(n))
+            {
+                return true;
+            }
+
+            this.results.Add(n, expr.ToString());
+            return this.results.Count < 100;
         }
     }
 }
