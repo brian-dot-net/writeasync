466ab1b976dd99bdeb680c792b4072de31783424
Brian Rogers <bobbymcr@gmail.com>
2018-04-02 20:27:04 -0700

    ExpressionSample - inital commit

---
+++ projects/ExpressionSample/GWExpr/Class1.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Class1.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace GWExpr
+{
+    public class Class1
+    {
+        private readonly string x;
+
+        public Class1(string x)
+        {
+            this.x = x;
+        }
+
+        public override string ToString() => this.x;
+    }
+}
