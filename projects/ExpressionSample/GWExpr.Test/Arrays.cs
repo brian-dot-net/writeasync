011a936f3782acebb8e3c0d8bf7125e2341a48f4
Brian Rogers <bobbymcr@gmail.com>
2018-04-03 17:03:27 -0700

    ExpressionSample - add test for string array 2D

--- projects/ExpressionSample/GWExpr.Test/Arrays.cs
+++ projects/ExpressionSample/GWExpr.Test/Arrays.cs
@@ -36,6 +36,15 @@ namespace GWExpr.Test
             Test(input, output);
         }
 
+        [InlineData("A$(1,0)", "ArrayVariable(StringVariable(A), NumericLiteral(1), NumericLiteral(0))")]
+        [InlineData("AB$(1,2)", "ArrayVariable(StringVariable(AB), NumericLiteral(1), NumericLiteral(2))")]
+        [InlineData("XYZ123$(250,99)", "ArrayVariable(StringVariable(XYZ123), NumericLiteral(250), NumericLiteral(99))")]
+        [Theory]
+        public void StringWithLiteralIndex2D(string input, string output)
+        {
+            Test(input, output);
+        }
+
         private static void Test(string input, string output)
         {
             BasicExpression.FromString(input).ToString().Should().Be(output);
