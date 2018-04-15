c02c73017382b993f0d546b589ec633cfea148f2
Brian Rogers <bobbymcr@gmail.com>
2018-04-15 12:26:32 -0700

    ExpressionSample - more PRINT tests (ignore case/spaces)

--- projects/ExpressionSample/GWParse.Test/Statements/Print.cs
+++ projects/ExpressionSample/GWParse.Test/Statements/Print.cs
@@ -14,5 +14,22 @@ namespace GWParse.Test.Statements
         {
             Test.Good(input, output);
         }
+
+        [InlineData("print", "Print()")]
+        [InlineData("PrInT", "Print()")]
+        [Theory]
+        public void IgnoreCase(string input, string output)
+        {
+            Test.Good(input, output);
+        }
+
+        [InlineData(" PRINT", "Print()")]
+        [InlineData("PRINT ", "Print()")]
+        [InlineData(" PRINT  ", "Print()")]
+        [Theory]
+        public void IgnoreSpaces(string input, string output)
+        {
+            Test.Good(input, output);
+        }
     }
 }
