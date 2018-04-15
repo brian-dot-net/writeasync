7a4422bb46ed022151fe9d724c7c61dc1f5a15e1
Brian Rogers <bobbymcr@gmail.com>
2018-04-15 07:19:03 -0700

    ExpressionSample - add GW.Statements projects

---
+++ projects/ExpressionSample/GW.Statements/Class1.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Class1.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace GW.Statements
+{
+    public class Class1
+    {
+        private readonly string v;
+
+        public Class1(string v)
+        {
+            this.v = v;
+        }
+
+        public override string ToString() => this.v;
+    }
+}
