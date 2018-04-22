34eb803954e32a1003e3d048b9bfc2eba932cf59
Brian Rogers <bobbymcr@gmail.com>
2018-04-22 12:35:32 -0700

    AdventureSample - support >= expr

---
+++ projects/AdventureSample/test/GWBas2CS.Test/GeExpressions.cs
@@ -0,0 +1,66 @@
+// <copyright file="GeExpressions.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace GWBas2CS.Test
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public sealed class GeExpressions
+    {
+        [Fact]
+        public void TwoNumericOperands()
+        {
+            const string Input = @"10 A=B>=2";
+            const string Expected = @"*
+    private bool Main()
+    {
+        this.Init();
+        A_n = (((B_n.CompareTo(2)) >= (0)) ? (-1) : (0));
+        return false;
+    }
+*";
+
+            string actual = Test.Translate("MyProg", Input);
+
+            actual.Should().Match(Expected);
+        }
+
+        [Fact]
+        public void TwoStringOperands()
+        {
+            const string Input = @"10 A=B$>=""x""";
+            const string Expected = @"*
+    private bool Main()
+    {
+        this.Init();
+        A_n = (((B_s.CompareTo(""x"")) >= (0)) ? (-1) : (0));
+        return false;
+    }
+*";
+
+            string actual = Test.Translate("MyProg", Input);
+
+            actual.Should().Match(Expected);
+        }
+
+        [Fact]
+        public void ThreeNumericOperands()
+        {
+            const string Input = @"10 A=B>=C>=1";
+            const string Expected = @"*
+    private bool Main()
+    {
+        this.Init();
+        A_n = ((((((B_n.CompareTo(C_n)) >= (0)) ? (-1) : (0)).CompareTo(1)) >= (0)) ? (-1) : (0));
+        return false;
+    }
+*";
+
+            string actual = Test.Translate("MyProg", Input);
+
+            actual.Should().Match(Expected);
+        }
+    }
+}
