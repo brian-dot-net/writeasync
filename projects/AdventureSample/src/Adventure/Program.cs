2fe37ae6aa2287c19890f3ea33c3ed75ce6172ea
Brian Rogers <bobbymcr@gmail.com>
2018-05-01 19:20:42 -0700

    AdventureSample - add Adventure stub project

---
+++ projects/AdventureSample/src/Adventure/Program.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Program.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Adventure
+{
+    internal sealed class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
