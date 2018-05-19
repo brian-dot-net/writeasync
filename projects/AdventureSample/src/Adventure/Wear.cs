f6540120e959fef302a75cc3d87016ca790f61f4
Brian Rogers <bobbymcr@gmail.com>
2018-05-18 18:39:36 -0700

    AdventureSample - move Wear class to top level

---
+++ projects/AdventureSample/src/Adventure/Wear.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="Wear.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace Adventure
+{
+    using System;
+
+    internal sealed class Wear
+    {
+        private readonly GameState state;
+        private readonly Action<string> print;
+
+        public Wear(GameState state, Action<string> print)
+        {
+            this.state = state;
+            this.print = print;
+        }
+
+        public VerbResult Unknown(ObjectId id)
+        {
+            this.print("YOU CAN'T WEAR THAT!");
+            return VerbResult.Idle;
+        }
+
+        public VerbResult Gloves(ObjectId id)
+        {
+            if (!this.state.Objects.IsHere(id, this.state.Map.CurrentRoom))
+            {
+                this.print("YOU DON'T HAVE THE GLOVES.");
+                return VerbResult.Idle;
+            }
+
+            this.print("YOU ARE NOW WEARING THE GLOVES.");
+            this.state.WearingGloves = true;
+            return VerbResult.Idle;
+        }
+    }
+}
--- projects/AdventureSample/src/Adventure/adventure.cs
+++ projects/AdventureSample/src/Adventure/adventure.cs
@@ -848,35 +848,4 @@ internal sealed class adventure
         state.Objects.Hide(id);
         return VerbResult.Idle;
     }
-
-    private sealed class Wear
-    {
-        private readonly GameState state;
-        private readonly Action<string> print;
-
-        public Wear(GameState state, Action<string> print)
-        {
-            this.state = state;
-            this.print = print;
-        }
-
-        public VerbResult Unknown(ObjectId id)
-        {
-            this.print("YOU CAN'T WEAR THAT!");
-            return VerbResult.Idle;
-        }
-
-        public VerbResult Gloves(ObjectId id)
-        {
-            if (!this.state.Objects.IsHere(id, state.Map.CurrentRoom))
-            {
-                this.print("YOU DON'T HAVE THE GLOVES.");
-                return VerbResult.Idle;
-            }
-
-            this.print("YOU ARE NOW WEARING THE GLOVES.");
-            this.state.WearingGloves = true;
-            return VerbResult.Idle;
-        }
-    }
 }
