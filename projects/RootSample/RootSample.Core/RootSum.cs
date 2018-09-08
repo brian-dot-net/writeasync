1055a32267a80c165f8e6542ddb8035d0b1c295b
Brian Rogers <bobbymcr@gmail.com>
2018-09-07 18:42:53 -0700

    RootSample - add RootSum struct

---
+++ projects/RootSample/RootSample.Core/RootSum.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="RootSum.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+
+namespace RootSample
+{
+    using System.Text;
+
+    public struct RootSum
+    {
+        private readonly RootTerm a;
+        private readonly RootTerm b;
+
+        public RootSum(RootTerm a, RootTerm b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(this.a.ToString());
+            if (!this.b.IsZero)
+            {
+                sb.Append('+');
+                sb.Append(this.b.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
