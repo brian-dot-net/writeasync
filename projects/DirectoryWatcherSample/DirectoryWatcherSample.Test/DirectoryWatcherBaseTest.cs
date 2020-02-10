eab1aead2af06d12b08e59202e79c05e45a95b83
Brian Rogers <bobbymcr@gmail.com>
2020-02-09 21:37:48 -0800

    DirectoryWatcherBase - SubscribeSameFileTwiceDifferentCase

--- projects/DirectoryWatcherSample/DirectoryWatcherSample.Test/DirectoryWatcherBaseTest.cs
+++ projects/DirectoryWatcherSample/DirectoryWatcherSample.Test/DirectoryWatcherBaseTest.cs
@@ -164,6 +164,19 @@ namespace DirectoryWatcherSample.Test
                 .WithMessage(@"A subscription for 'X:\root\file1.txt' already exists.");
         }
 
+        [TestMethod]
+        public void SubscribeSameFileTwiceDifferentCase()
+        {
+            DirectoryWatcherBase watcherBase = new FakeDirectoryWatcher(new DirectoryInfo(@"X:\root"));
+            Action<FileInfo> onUpdate = f => { };
+            watcherBase.Subscribe("file1.txt", onUpdate);
+
+            Action act = () => watcherBase.Subscribe("FILE1.txt", onUpdate);
+
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage(@"A subscription for 'X:\root\FILE1.txt' already exists.");
+        }
+
         private sealed class FakeDirectoryWatcher : DirectoryWatcherBase
         {
             public FakeDirectoryWatcher(DirectoryInfo path)
