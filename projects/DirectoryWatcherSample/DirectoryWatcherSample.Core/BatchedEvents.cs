a43e929e73cd41a022b10b610cc53f86261c0ca6
Brian Rogers <bobbymcr@gmail.com>
2020-02-19 14:31:56 -0800

    BatchedEvents - AddOneAndComplete

--- projects/DirectoryWatcherSample/DirectoryWatcherSample.Core/BatchedEvents.cs
+++ projects/DirectoryWatcherSample/DirectoryWatcherSample.Core/BatchedEvents.cs
@@ -9,12 +9,28 @@ namespace DirectoryWatcherSample
 
     public sealed class BatchedEvents<T>
     {
+        private readonly Func<Task> delay;
+
+        private Action<T> callback;
+
         public BatchedEvents(Func<Task> delay)
         {
+            this.delay = delay;
+        }
+
+        public void Subscribe(T item, Action<T> callback)
+        {
+            this.callback = callback;
         }
 
         public void Add(T item, TimePoint timestamp)
         {
+            this.OnBatchCreated(item);
+        }
+
+        private void OnBatchCreated(T item)
+        {
+            this.delay().ContinueWith(t => this.callback(item));
         }
     }
 }
--- projects/DirectoryWatcherSample/DirectoryWatcherSample.Test/BatchedEventsTest.cs
+++ projects/DirectoryWatcherSample/DirectoryWatcherSample.Test/BatchedEventsTest.cs
@@ -5,6 +5,7 @@
 namespace DirectoryWatcherSample.Test
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -20,6 +21,23 @@ namespace DirectoryWatcherSample.Test
             Action act = () => events.Add("item1", new TimePoint(1));
 
             act.Should().NotThrow();
+        }
+
+        [TestMethod]
+        public void AddOneAndComplete()
+        {
+            TaskCompletionSource<bool> pending = new TaskCompletionSource<bool>();
+            BatchedEvents<string> events = new BatchedEvents<string>(() => pending.Task);
+            List<string> batches = new List<string>();
+
+            events.Subscribe("item1", i => batches.Add(i));
+            events.Add("item1", new TimePoint(1));
+
+            batches.Should().BeEmpty();
+
+            pending.SetResult(true);
+
+            batches.Should().ContainSingle().Which.Should().Be("item1");
         }
     }
 }
